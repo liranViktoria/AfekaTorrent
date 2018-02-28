e68bda22e177f9dd854d19e9b3d7ed002575a13b
liranViktoria <36151666+liranViktoria@users.noreply.github.com>
2018-02-28 14:21:36 +0200

    28.2.18

---
+++ AfekaTorrentServer/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+using System.ServiceModel;
+using System.Net;
+using System.Net.Sockets;
+
+
+
+
+namespace AfekaTorrentServer
+{
+
+    class Program
+    {
+        private static ServiceHost server;
+
+        static void Main(string[] args)
+        {
+            IPEndPoint meetingPoint = new IPEndPoint(IPAddress.Loopback, 8005);
+            TcpListener listener = new TcpListener(meetingPoint.Port);
+            listener.Start();
+            Console.WriteLine("server started!");
+            Console.Read();
+        }
+
+
+    }
+}
--- WpfApplication/MainWindow.xaml.cs
+++ WpfApplication/MainWindow.xaml.cs
@@ -1,6 +1,13 @@
 ﻿
+using System;
+using System.IO;
+using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Windows;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+ 
 namespace WpfApplication
 {
 
@@ -10,17 +17,29 @@ namespace WpfApplication
     /// </summary>
     public partial class MainWindow 
     {
-        private Socket s;
-        private DetailsFromUser details;
+        public static String fileName = "MyConfig.xml";
+        private DetailsFromUser details = new DetailsFromUser();
+        private XmlSerializer serializerObj;
+        private Socket s ;
+       
 
         public MainWindow()
         {
-            
             InitializeComponent();
-            details.UserName = user_name_texbox.Text;
-            details.Password = password_box.Password;
+
             s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            s.Connect("127.0.0.1", 8080);
+           
+            s.Connect("127.0.0.1", 8005);
+        }
+
+        private void SaveToFile()
+        {
+            using(var srFileStream = new FileStream(fileName, FileMode.Create))
+            {
+                serializerObj = new XmlSerializer(typeof(DetailsFromUser));
+                serializerObj.Serialize(srFileStream, details);
+
+            }
         }
 
         private void button_path_upload_Click(object sender, RoutedEventArgs e)
@@ -42,6 +61,73 @@ namespace WpfApplication
                 success_download.Visibility = Visibility.Visible;
             }
         }
+
+        private void updateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            details.UserName = user_name_texbox.Text;
+            details.Password = password_box.Password;
+            details.PathDownload = download_path_texbox.Text;
+            details.PathUpload = upload_path_texbox.Text;
+            details.PortIn = 8005;
+            try
+            {
+                InputChacker(details.UserName, details.Password,details.PathDownload,details.PathUpload);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Confirmation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            details.IpLocalHost = GetIpAddress().ToString();
+            GetFilesFromPath();
+            SaveToFile();
+
+
+
+
+        }
+
+        private void InputChacker(string userNameCheck, string passwordCheck, string pathDownload, string pathUpload)
+        {
+            if(userNameCheck == "" && passwordCheck == "")
+                throw new Exception("You must enter username and password before uploading...");
+            else if (userNameCheck == "")
+                throw new Exception("You must enter username before uploading...");
+            else if( passwordCheck == "")
+                throw  new Exception("You must enter password before uploading...");
+            else if(pathDownload == "" && pathUpload == "")
+                throw new Exception("You must choose paths...");
+            else if(pathDownload == "")
+                throw new Exception("You must choose download path...");
+            else if(pathUpload == "")
+                throw new Exception("You must choose upload path...");
+        }
+
+        public static IPAddress GetIpAddress()
+        {
+            IPAddress[] hostAddresses = Dns.GetHostAddresses("");
+            foreach (IPAddress hostAddress in hostAddresses)
+            {
+                if (hostAddress.AddressFamily == AddressFamily.InterNetwork &&
+                    !IPAddress.IsLoopback(hostAddress) &&  !hostAddress.ToString().StartsWith("169.254."))  
+                    return hostAddress;
+            }
+            return null;
+        }
+
+        public void GetFilesFromPath()
+        {
+            var files = new DirectoryInfo(details.PathUpload).GetFiles();
+            for (int i = 0; i < files.Length; i++)
+            {
+              details.AddFile(files[i].Name,files[i].Length);
+            }
+
+
+
+        }
+
+
     }
 }
 
